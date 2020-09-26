8165e5afee28fb89d1c1161e731cd3dee80326a5
Ornella-KK <ornellakankazi@gmail.com>
2020-09-26 19:50:18 +0200

    deleted some errors

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,13 @@
-function getInput({
-    var inputMonth=parseInt(document.grtElementById("month").value);
+function getInput(){
+    var inputMonth=parseInt(document.getElementById("month").value);
     var inputDate= parseInt(document.getElementById("date").value);
-    var inputYear=parseInt(document.getElementById("year").value);
+    var inputYear=parseInt(document.getElementById("year");
  if(inputMonth>12 || inputMonth==0){
-     alert("Invalid year");
+     alert("Invalid month");
      return false;
  }else if(inputDate>31 || inputMonth==0){
-     alert("Invalid month")
+     alert("Invalid date")
      return false;
  }
 }
+function 
