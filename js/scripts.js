d7799ded3517e1cbb81cd47e90d7af41daa8e12c
Ornella-KK <ornellakankazi@gmail.com>
2020-09-26 19:29:31 +0200

    add conditions

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,11 @@ function getInput({
     var inputMonth=parseInt(document.grtElementById("month").value);
     var inputDate= parseInt(document.getElementById("date").value);
     var inputYear=parseInt(document.getElementById("year").value);
-    
+ if(inputMonth>12 || inputMonth==0){
+     alert("Invalid year");
+     return false;
+ }else if(inputDate>31 || inputMonth==0){
+     alert("Invalid month")
+     return false;
+ }
 }
