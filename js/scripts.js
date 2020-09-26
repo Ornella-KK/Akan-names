129e5af35b423ef07edb1d03f534d35469c8a320
Ornella-KK <ornellakankazi@gmail.com>
2020-09-26 19:11:37 +0200

    add getinput function and variables

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+function getInput({
+    var inputMonth=parseInt(document.grtElementById("month").value);
+    var inputDate= parseInt(document.getElementById("date").value);
+    var inputYear=parseInt(document.getElementById("year").value);
+    
+}
