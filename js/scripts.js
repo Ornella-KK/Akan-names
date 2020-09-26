cbff7a8329432fff65557d5ed530c977496ece29
Ornella-KK <ornellakankazi@gmail.com>
2020-09-26 19:58:18 +0200

    correction of errors

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 function getInput(){
+    var inputCentury=parseInt(document.getElementById("century").value);
     var inputMonth=parseInt(document.getElementById("month").value);
     var inputDate= parseInt(document.getElementById("date").value);
-    var inputYear=parseInt(document.getElementById("year");
+    var inputYear=parseInt(document.getElementById("year").value);
  if(inputMonth>12 || inputMonth==0){
      alert("Invalid month");
      return false;
