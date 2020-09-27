9e3f254fd40da0b59601420391fc6e148b9e719a
Ornella-KK <ornellakankazi@gmail.com>
2020-09-27 11:23:50 +0200

    add a validation for gender

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 function getInput(){
-    var inputCentury=parseInt(document.getElementById("century").value);
+    var inputCentury=parseInt(document.getElementById("century");
     var inputMonth=parseInt(document.getElementById("month").value);
     var inputDate= parseInt(document.getElementById("date").value);
-    var inputYear=parseInt(document.getElementById("year").value);
+    var inputYear=parseInt(document.getElementById("year");
  if(inputMonth>12 || inputMonth==0){
      alert("Invalid month");
      return false;
@@ -11,4 +11,11 @@ function getInput(){
      return false;
  }
 }
-function 
+function validate(form){
+    var gMale=form.gender_male.value;
+    var gFemale=form.gender_female.value;
+    if(gMale.checked==false && gFemale.checked==false){
+        alert("You must select male or female");
+        return false;
+    }
+}
